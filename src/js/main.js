445001ebca56207f44df62189109db2744fdae6a
MateuszRuszczynski <mateusz.ruszczynski@gmail.com>
2020-02-18 21:17:02 +0100

    Feat: add mobile styles

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,19 @@ tabs.forEach(tab => {
     const target = document.querySelector(tab.dataset.tab)
     tabContents.forEach(tabContent => {
       tabContent.classList.remove('active')
-    })
+    });
     tabs.forEach(tab => {
       tab.classList.remove('active')
-    })
+    });
     tab.classList.add('active')
     target.classList.add('active')
-  })
+  });
+});
+
+//////////////HAMBURGER
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('.navigation__list');
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('hamburger--active');
+  nav.classList.toggle('navigation__list--active');
 })
